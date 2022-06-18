e47418c695eada54238b62b929d762f3aedaca78
Gustavo Reis <gustavogk09@users.noreply.github.com>
2022-06-18 19:46:18 -0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Projeto de estudo Soundpad:
+- Utilizando de HTML, CSS e JavaScript;
+- Design aplicado para que fique mais amigável;
+- Feito totalmente por mim afim de aprender usar Funções, e outras funcionalidades como o "addEventListener";
+- E para me divertir também!
---
+++ script.js
@@ -0,0 +1,44 @@
+function sound1() {
+    let ding = new Audio('a1.mp3');
+	ding.play();
+}
+
+function sound2() {
+    let ding = new Audio('a2.mp3');
+	ding.play();
+}
+
+function sound3() {
+    let ding = new Audio('a3.mp3');
+	ding.play();
+}
+function sound4() {
+    let ding = new Audio('a4.mp3');
+	ding.play();
+}
+function sound5() {
+    let ding = new Audio('a5.mp3');
+	ding.play();
+}
+function sound6() {
+    let ding = new Audio('a6.mp3');
+	ding.play();
+}
+function sound7() {
+    let ding = new Audio('a7.mp3');
+	ding.play();
+}
+
+function sound8() {
+    let ding = new Audio('a8.mp3');
+	ding.play();
+}
+
+ad1.addEventListener("click", sound1);
+ad2.addEventListener("click", sound2);
+ad3.addEventListener("click", sound3);
+ad4.addEventListener("click", sound4);
+ad5.addEventListener("click", sound5);
+ad6.addEventListener("click", sound6);
+ad7.addEventListener("click", sound7);
+ad8.addEventListener("click", sound8);
